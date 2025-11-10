6f7b6483d021805a3790d6bf0dff9d3e186ab05b
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-10 22:12:00 +0100

    fixed bug where same values were skipped

--- src/algoritms/sorting/quick_sort.rs
+++ src/algoritms/sorting/quick_sort.rs
@@ -5,10 +5,15 @@ pub fn quick_sort(input_list: &[i32]) -> Vec<i32> {
     let pivot = input_list[input_list.len() / 2];
     let less: Vec<i32> = input_list.iter().filter(|&&x| x < pivot).copied().collect();
     let greater: Vec<i32> = input_list.iter().filter(|x| **x > pivot).copied().collect();
+    let equal: Vec<i32> = input_list
+        .iter()
+        .filter(|&&x| x == pivot)
+        .copied()
+        .collect();
 
     quick_sort(&less)
         .into_iter()
-        .chain(vec![pivot])
+        .chain(equal)
         .chain(quick_sort(&greater))
         .collect()
 }
@@ -22,5 +27,11 @@ mod test {
         let result = quick_sort(&list);
         assert_eq!(result, [1, 2, 3, 4, 5])
     }
-    
+
+    #[test]
+    fn test_quicksort_positive_number_duplicates() {
+        let list = vec![2, 4, 1, 3, 5, 3];
+        let result = quick_sort(&list);
+        assert_eq!(result, [1, 2, 3, 3, 4, 5])
+    }
 }
