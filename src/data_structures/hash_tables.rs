852f17680cc08457950bee2bb7be881aadf3dc1b
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-12 23:36:39 +0100

    added resize logic

--- src/data_structures/hash_tables.rs
+++ src/data_structures/hash_tables.rs
@@ -3,17 +3,15 @@ use std::collections::btree_map::Keys;
 use std::fmt::Debug;
 use std::str::Chars;
 
-#[derive(Debug)]
-pub enum MoMapError {
-    CreationError,
-    ItemNotFound,
-}
 pub struct MoMap<V> {
     bucket_item_count: u32,
     buckets: Vec<Vec<(String, V)>>,
 }
 
-impl<V: Clone + Copy + Debug> MoMap<V> {
+impl<V> MoMap<V>
+where
+    V: Clone,
+{
     pub fn new() -> Self {
         let buckets = vec![Vec::new(); 8];
         MoMap {
@@ -22,17 +20,27 @@ impl<V: Clone + Copy + Debug> MoMap<V> {
         }
     }
 
-    pub fn bla(&self) {
+    pub fn bla(&self)
+    where
+        V: Debug,
+    {
         println!("size of bucket: {}", self.buckets.len());
         println!("amount of items added: {}", self.bucket_item_count);
         println!("amount of items added: {:?}", self.buckets);
     }
 
-    pub fn insert(&mut self, key: String, value: V) -> Result<(), MoMapError> {
+    pub fn insert(&mut self, key: String, value: V) -> Option<V> {
+        self.resize();
         let index: usize = self.hashing_function(key.chars());
-        self.buckets[index].push((key, value));
-        self.bucket_item_count += 1;
-        Ok(())
+        let bucket = &mut self.buckets[index];
+        if let Some(existing) = bucket.iter_mut().find(|x| x.0 == key) {
+            let old_value = std::mem::replace(&mut existing.1, value);
+            Some(old_value)
+        } else {
+            self.buckets[index].push((key, value));
+            self.bucket_item_count += 1;
+            None
+        }
     }
 
     pub fn get(&self, key: &str) -> Option<&V> {
@@ -43,16 +51,16 @@ impl<V: Clone + Copy + Debug> MoMap<V> {
             .map(|x| &x.1)
     }
 
-    pub fn remove(&mut self, key: &str) -> Result<String, MoMapError> {
+    pub fn remove(&mut self, key: &str) -> Option<V> {
         let index: usize = self.hashing_function(key.chars());
         let bucket = &mut self.buckets[index];
 
         match bucket.iter().position(|x| x.0 == key) {
             Some(v) => {
                 self.bucket_item_count -= 1;
-                Ok(bucket.swap_remove(v).0)
+                Some(bucket.swap_remove(v).1)
             }
-            None => Err(MoMapError::ItemNotFound),
+            None => None,
         }
     }
 
@@ -60,5 +68,24 @@ impl<V: Clone + Copy + Debug> MoMap<V> {
         let sum: usize = chars.map(|c| c as usize).sum();
 
         sum % self.buckets.len()
+    }
+
+    fn resize(&mut self) {
+        let count = self.buckets.iter().filter(|x| !x.is_empty()).count() as f64;
+
+        if count / self.buckets.len() as f64 >= 0.7 {
+            let mut new_buckets: Vec<Vec<(String, V)>> = vec![Vec::new(); self.buckets.len() * 2];
+            self.bucket_item_count = 0;
+            self.buckets.clone().iter().for_each(|x| {
+                x.clone().iter().for_each(|x| {
+                    let clones_value = x.clone();
+                    let index: usize = self.hashing_function(clones_value.0.chars());
+                    new_buckets[index].push((clones_value.0, clones_value.1));
+                    self.bucket_item_count += 1;
+                })
+            });
+
+            self.buckets = new_buckets
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,45 @@ use algoritms::search::binary_search;
 use algoritms::sorting::quick_sort::quick_sort;
 
 use crate::algoritms::sorting::bubble_sort::bubble_sort;
-use crate::data_structures::hash_tables::{MoMap, MoMapError};
+use crate::data_structures::hash_tables::MoMap;
 
-fn main() -> Result<(), MoMapError> {
+fn main() {
     let mut my_map: MoMap<u32> = MoMap::new();
 
     my_map.bla();
 
     my_map.insert("hello".to_string(), 32);
+    my_map.bla();
     my_map.insert("hello1".to_string(), 32);
+    my_map.bla();
     my_map.insert("aaaa".to_string(), 42);
+    my_map.bla();
     my_map.insert("mohamad".to_string(), 11);
+    my_map.bla();
+    my_map.insert("mohasbcdqdasdaamad".to_string(), 11);
+    my_map.bla();
+    my_map.insert("ddddddd".to_string(), 11);
+    my_map.bla();
+    my_map.insert("ddddaaaaaaddd".to_string(), 11);
+    my_map.bla();
+    my_map.insert("llllllll".to_string(), 11);
+    my_map.bla();
+    my_map.insert("kkkkkkkk".to_string(), 11);
+    my_map.bla();
+    my_map.insert("k".to_string(), 11);
+    my_map.bla();
+    my_map.insert("a".to_string(), 11);
+    my_map.bla();
+    my_map.insert("b".to_string(), 11);
+    my_map.bla();
+    my_map.insert("c".to_string(), 11);
     my_map.bla();
     let test = my_map.get("mohamad").unwrap();
 
     println!("woow: {}", test);
-    my_map.remove("mohamad")?;
+    // my_map.remove("mohamad");
     let test = my_map.get("mohamad");
     println!("woow2: {:?}", test);
+    my_map.insert("aaaa".to_string(), 123);
     my_map.bla();
-    Ok(())
 }
