1dfc780c6a36b0816d840b8fdf7019e1eb2dde37
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-19 17:12:54 +0100

    better impl, can now check if self is valid in bfs

--- src/data_structures/graphs.rs
+++ src/data_structures/graphs.rs
@@ -3,29 +3,39 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Debug, Clone)]
-pub struct Profile {
+pub struct Node {
     pub name: String,
     pub is_seller: bool,
 }
 
 #[derive(Debug)]
 pub struct MoGraph {
-    adjacency_list: HashMap<String, Vec<Profile>>,
+    nodes: HashMap<String, Node>,
+    adjacency_list: HashMap<String, Vec<String>>,
 }
 
 impl MoGraph {
     pub fn new() -> Self {
         Self {
+            nodes: HashMap::new(),
             adjacency_list: HashMap::new(),
         }
     }
 
-    pub fn add_edge(&mut self, from: String, to: Profile) {
+    pub fn add_node(&mut self, node: Node) {
+        self.nodes.entry(node.name.clone()).or_insert(node);
+    }
+
+    pub fn add_edge(&mut self, from: String, to: String) {
         self.adjacency_list.entry(from).or_default().push(to);
     }
 
-    pub fn neighbors(&self, node: &str) -> Option<&Vec<Profile>> {
+    pub fn neighbors(&self, node: &str) -> Option<&Vec<String>> {
         self.adjacency_list.get(node)
+    }
+
+    pub fn get_node(&self, key: &str) -> Option<&Node> {
+        self.nodes.get(key)
     }
 
     /// Breadth-first search
@@ -38,26 +48,27 @@ impl MoGraph {
     ///
     /// Returns `Some(Profile)` if a node matching the condition is found, `None` otherwise.
     ///
-    pub fn bfs<F>(&self, start: &str, condition: F) -> Option<Profile>
+    pub fn bfs<F>(&self, start: &str, condition: F) -> Option<&Node>
     where
-        F: Fn(&Profile) -> bool,
+        F: Fn(&Node) -> bool,
     {
         let mut queue = VecDeque::new();
         let mut visited = HashSet::new();
 
         queue.push_back(start.to_string());
 
-        while let Some(person) = queue.pop_front() {
-            if visited.contains(&person) {
-                continue;
+        while let Some(key) = queue.pop_front() {
+            if let Some(node) = self.nodes.get(&key) {
+                if condition(node) {
+                    return Some(&node);
+                }
             }
-            visited.insert(person.clone());
-            if let Some(neighbors) = self.neighbors(&person) {
+            if let Some(neighbors) = self.neighbors(&key) {
                 for neighbor in neighbors {
-                    if condition(neighbor) {
-                        return Some(neighbor.clone());
+                    if !visited.contains(neighbor) {
+                        visited.insert(neighbor.clone());
+                        queue.push_back(neighbor.clone());
                     }
-                    queue.push_back(neighbor.name.clone());
                 }
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,8 @@ mod algoritms;
 mod data_structures;
 
 // use crate::data_structures::hash_tables::MoMap;
-use crate::data_structures::graphs::{MoGraph, Profile};
+use crate::data_structures::graphs::{MoGraph, Node};
 
 fn main() {
-    let mut mo_graph = MoGraph::new();
-    mo_graph.add_edge(
-        "Mo".to_string(),
-        Profile {
-            name: "bob".to_string(),
-            is_seller: false,
-        },
-    );
-    mo_graph.add_edge(
-        "Mo".to_string(),
-        Profile {
-            name: "Moh".to_string(),
-            is_seller: true,
-        },
-    );
-    mo_graph.add_edge(
-        "Moh".to_string(),
-        Profile {
-            name: "Susan".to_string(),
-            is_seller: false,
-        },
-    );
-    mo_graph.add_edge(
-        "Susan".to_string(),
-        Profile {
-            name: "henry".to_string(),
-            is_seller: true,
-        },
-    );
-
-    println!("{:?}", mo_graph);
-
-    let seller = mo_graph.bfs("Mo", |x| x.is_seller);
-
-    println!("The first seller is: {:?}", seller)
+    println!("hello")
 }
