e79abe70409c9cda65b16cb2659e8bd0d0118aca
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-20 16:44:52 +0100

    tree done for now

--- src/data_structures/tree.rs
+++ src/data_structures/tree.rs
@@ -7,3 +7,33 @@ struct TreeNode {
     right: Option<Rc<RefCell<TreeNode>>>,
 }
 
+impl TreeNode {
+    // Leaf, must have a char.
+    pub fn new_leaf(character: char, frequency: usize) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            character: Some(character),
+            frequency,
+            left: None,
+            right: None,
+        }))
+    }
+
+    // internal node, no char just connects to other nodes
+    pub fn new_internal(
+        frequency: usize,
+        left: Rc<RefCell<TreeNode>>,
+        right: Rc<RefCell<TreeNode>>,
+    ) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            character: None,
+            frequency,
+            left: Some(left),
+            right: Some(right),
+        }))
+    }
+
+    pub fn is_leaf(&self) -> bool {
+        self.left.is_none() && self.right.is_none()
+    }
+}
+
