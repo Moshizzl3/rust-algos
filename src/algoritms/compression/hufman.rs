061ad3e95625e5d012e3a4d270414e6d0c0653d4
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-20 17:43:28 +0100

    added function to build the huffmantree from a text

--- src/algoritms/compression/hufman.rs
+++ src/algoritms/compression/hufman.rs
@@ -2,15 +2,49 @@
 use crate::data_structures::tree::TreeNode;
 use std::cmp::Ordering;
 
+use std::collections::BinaryHeap;
 use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-pub fn count_frequencies(text: &str) -> HashMap<char, i32> {
+pub fn count_frequencies(text: &str) -> HashMap<char, usize> {
     let mut freq_map = HashMap::new();
 
     text.chars()
         .for_each(|c| *freq_map.entry(c).or_insert(0) += 1);
 
     freq_map
+}
+
+pub fn build_hufman_tree(text: &str) -> Option<Rc<RefCell<TreeNode>>> {
+    let freq_map = count_frequencies(text);
+    if freq_map.is_empty() {
+        return None;
+    }
+
+    //Create the leafs and add to tree
+
+    let mut heap = BinaryHeap::new();
+    for (ch, freq) in freq_map {
+        let leaf = TreeNode::new_leaf(ch, freq);
+        heap.push(HeapNode { node: leaf });
+    }
+
+    // Build tree
+
+    while heap.len() > 1 {
+        // pop smallest values of the heap - unwrap for now
+        let left = heap.pop().unwrap().node;
+        let right = heap.pop().unwrap().node;
+
+        // create the parent
+        let combined = left.borrow().frequency + right.borrow().frequency;
+        let parent = TreeNode::new_internal(combined, left, right);
+
+        //push back
+        heap.push(HeapNode { node: parent });
+    }
+
+    //root node
+    heap.pop().map(|heap_node| heap_node.node)
 }
 
 pub struct HeapNode {
