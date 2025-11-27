b7ec27bfb473b695e7f312bab7c650642196d8fe
Mohamad Ibrahim <81045658+Moshizzl3@users.noreply.github.com>
2025-11-27 18:22:29 +0100

    added split node logic

--- src/data_structures/b_tree.rs
+++ src/data_structures/b_tree.rs
@@ -1,5 +1,6 @@
 // B tree 🐝 (specialized m-way tree)
 
+use core::borrow;
 use std::{cell::RefCell, rc::Rc};
 
 type NodeRef<K, V> = Rc<RefCell<BTreeNode<K, V>>>;
@@ -35,6 +36,33 @@ impl<K: Ord + Clone, V: Clone> BTree<K, V> {
         } else {
             None
         }
+    }
+
+ 
+
+
+    fn split_node(node: &NodeRef<K, V>) -> (K, V, NodeRef<K, V>) {
+        let mut borrowed = node.borrow_mut();
+        let median = borrowed.keys.len() / 2;
+
+        let right_keys = borrowed.keys.split_off(median + 1);
+        let right_values = borrowed.values.split_off(median + 1);
+
+        let median_key = borrowed.keys.pop().unwrap();
+        let median_value = borrowed.values.pop().unwrap();
+
+        let right_children = borrowed
+            .children
+            .as_mut()
+            .map(|children| children.split_off(median + 1));
+
+        let right_node = Rc::new(RefCell::new(BTreeNode {
+            keys: right_keys,
+            values: right_values,
+            children: right_children,
+        }));
+
+        (median_key, median_value, right_node)
     }
 
     fn search_helper(node: &NodeRef<K, V>, key: &K) -> Option<V> {
